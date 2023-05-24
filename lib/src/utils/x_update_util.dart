f81c8dd9b0cf5a38d1b07deec47b9dd33280e3c3
ilovesshan <2665939276@qq.com>
2023-05-24 22:19:43 +0800

    refactor: XUpdateUtil code formatter

--- lib/src/utils/x_update_util.dart
+++ lib/src/utils/x_update_util.dart
@@ -3,47 +3,36 @@ import 'dart:io';
 import 'package:common_utils_v2/common_utils_v2.dart';
 
 class XUpdateUtil {
-  static const String _baseUrl = "https://xxxx.com";
-
   /// 初始化
   static void initXUpdate() {
     if (Platform.isAndroid) {
       FlutterXUpdate.init(
-
-        ///是否输出日志
-          debug: true,
-
-          ///是否使用post请求
-          isPost: false,
-
-          ///post请求是否是上传json
-          isPostJson: false,
-
-          ///请求响应超时时间
-          timeout: 25000,
-
-          ///是否开启自动模式
-          isWifiOnly: false,
-
-          ///是否开启自动模式
-          isAutoMode: false,
-
-          ///需要设置的公共参数
-          supportSilentInstall: false,
-
-          ///在下载过程中，如果点击了取消的话，是否弹出切换下载方式的重试提示弹窗
-          enableRetry: false)
-          .then((value) {
-        printLog(StackTrace.current, "Xupdate初始化成功: $value");
+              ///是否输出日志
+              debug: true,
+              ///是否使用post请求
+              isPost: false,
+              ///post请求是否是上传json
+              isPostJson: false,
+              ///请求响应超时时间
+              timeout: 25000,
+              ///是否开启自动模式
+              isWifiOnly: false,
+              ///是否开启自动模式
+              isAutoMode: false,
+              ///需要设置的公共参数
+              supportSilentInstall: false,
+              ///在下载过程中，如果点击了取消的话，是否弹出切换下载方式的重试提示弹窗
+              enableRetry: false).then((value) {
+        Log.d("Xupdate初始化成功: $value");
       }).catchError((error) {
-        printLog(StackTrace.current, "Xupdate初始化失败: $error");
+        Log.d("Xupdate初始化失败: $error");
       });
 
       FlutterXUpdate.setErrorHandler(onUpdateError: (Map<String, dynamic>? message) async {
-        printLog(StackTrace.current, "Xupdate初始化失败: $message");
+        Log.d("Xupdate初始化失败: $message");
       });
     } else {
-      printLog(StackTrace.current, "ios暂不支持XUpdate更新");
+      Log.d("ios暂不支持XUpdate更新");
     }
   }
 
@@ -56,7 +45,8 @@ class XUpdateUtil {
         versionCode: updateEntity.versionCode,
         versionName: updateEntity.versionName,
         updateContent: updateEntity.updateContent,
-        downloadUrl: _baseUrl + "${updateEntity.downloadUrl}",
-        apkSize: updateEntity.apkSize);
+        downloadUrl: HttpHelperUtil.baseurl + "${updateEntity.downloadUrl}",
+        apkSize: updateEntity.apkSize,
+    );
   }
 }
