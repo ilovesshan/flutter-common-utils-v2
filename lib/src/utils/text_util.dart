4c2d76713ab08430c5fa91167856d5101b9510a2
ilovesshan <2665939276@qq.com>
2023-05-25 14:03:45 +0800

    feat: add String Api method

--- lib/src/utils/text_util.dart
+++ lib/src/utils/text_util.dart
@@ -13,6 +13,31 @@ class TextUtil {
   /// 判断数据是否是空 如果空则给予一个默认值返回
   static String isEmptyWith(String text, String defaultValue) {
     text = text.trim();
-    return ( text.isEmpty || text == null || text == "null") ? defaultValue : text ;
+    return (text.isEmpty || text == null || text == "null") ? defaultValue : text;
+  }
+
+  /// 将指定位置的字符串替换成****
+  static String hideNumber(String phoneNo, {int start = 3, int end = 7, String replacement = '****'}) {
+    return phoneNo.replaceRange(start, end, replacement);
+  }
+
+  /// 字符串替换
+  static String replace(String text, Pattern pattern, String replace) {
+    return text.replaceAll(pattern, replace);
+  }
+
+  /// 字符串分割
+  static List<String> split(String text, Pattern pattern) {
+    return text.split(pattern);
+  }
+
+  /// 字符串反转
+  static String reverse(String text) {
+    if (isEmpty(text)) return '';
+    StringBuffer sb = StringBuffer();
+    for (int i = text.length - 1; i >= 0; i--) {
+      sb.writeCharCode(text.codeUnitAt(i));
+    }
+    return sb.toString();
   }
 }
