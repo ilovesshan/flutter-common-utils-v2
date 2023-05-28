fe9f32573f5486460b44cbd4d91d78b737f7ecdc
ilovesshan <2665939276@qq.com>
2023-05-28 19:36:15 +0800

    refactor:  common appbar style

--- lib/src/widgets/common_app_bar.dart
+++ lib/src/widgets/common_app_bar.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+
 import 'package:common_utils_v2/common_utils_v2.dart';
 
 class CommonAppBar {
   /// 简朴白
-  static AppBar show(String text, { List<Widget>? actions, PreferredSizeWidget? bottom}){
+  static AppBar show(String text, {List<Widget>? actions, PreferredSizeWidget? bottom, double? elevation}) {
     return AppBar(
       systemOverlayStyle: const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark),
-      title: Text(text, style: TextStyle(fontSize: 14.sp, color: const Color(0xFF222222), fontWeight: FontWeight.bold)),backgroundColor: Colors.white, centerTitle: true, elevation: 0,
-      leading: GestureDetector(
-        child: Container(padding: EdgeInsets.all(20.w), child: Image.asset("assets/common/nav-back-black.png")),
-        onTap: ()=> Get.back(),
-      ),
+      title: Text(text, style: TextStyle(fontSize: 18.sp, color: const Color(0xFF222222))),
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      elevation: elevation ?? 0,
+      leading: IconButton(icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 22), onPressed: () => Get.back()),
       actions: actions,
       bottom: bottom,
     );
   }
 
   /// 主题色
-  static AppBar showWidthPrimaryTheme(String text, { List<Widget>? actions, PreferredSizeWidget? bottom}){
+  static AppBar showWidthPrimaryTheme(String text, {List<Widget>? actions, PreferredSizeWidget? bottom, double? elevation}) {
     return AppBar(
       systemOverlayStyle: const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.light),
-      title: Text(text, style: TextStyle(fontSize: 14.sp, color: const Color(0xFFFFFFFF), fontWeight: FontWeight.bold)),backgroundColor: Get.theme.primaryColor, centerTitle: true, elevation: 0,
-      leading: GestureDetector(
-        child: Container(padding: EdgeInsets.all(20.w), child: Image.asset("assets/common/nav-back-white.png")),
-        onTap: ()=> Get.back(),
-      ),
+      title: Text(text, style: TextStyle(fontSize: 18.sp, color: const Color(0xFFFFFFFF))),
+      backgroundColor: Get.theme.primaryColor,
+      centerTitle: true,
+      elevation: elevation ?? 0,
+      leading: IconButton(icon: const Icon(Icons.arrow_back_ios, color: Colors.white, size: 22), onPressed: () => Get.back()),
       actions: actions,
       bottom: bottom,
     );
