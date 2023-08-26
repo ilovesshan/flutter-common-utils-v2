f1070bb5a45b93e29ee4841607ecfb0fd52b854a
ilovesshan <ilovesshany@gmail.com>
2023-08-26 13:47:12 +0800

    refactor: add leading options fro AppBar

--- lib/src/widgets/common_app_bar.dart
+++ lib/src/widgets/common_app_bar.dart
@@ -5,28 +5,28 @@ import 'package:common_utils_v2/common_utils_v2.dart';
 
 class CommonAppBar {
   /// 简朴白
-  static AppBar show(String text, {List<Widget>? actions, PreferredSizeWidget? bottom, double? elevation}) {
+  static AppBar show(String text, {Widget? leading, List<Widget>? actions, PreferredSizeWidget? bottom, double? elevation}) {
     return AppBar(
       systemOverlayStyle: const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark),
       title: Text(text, style: TextStyle(fontSize: 18.sp, color: const Color(0xFF222222))),
       backgroundColor: Colors.white,
       centerTitle: true,
       elevation: elevation ?? 0,
-      leading: IconButton(icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 22), onPressed: () => Get.back()),
+      leading: leading ?? IconButton(icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 22), onPressed: () => Get.back()),
       actions: actions,
       bottom: bottom,
     );
   }
 
   /// 主题色
-  static AppBar showWidthPrimaryTheme(String text, {List<Widget>? actions, PreferredSizeWidget? bottom, double? elevation}) {
+  static AppBar showWidthPrimaryTheme(String text, {Widget? leading, List<Widget>? actions, PreferredSizeWidget? bottom, double? elevation}) {
     return AppBar(
       systemOverlayStyle: const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.light),
       title: Text(text, style: TextStyle(fontSize: 18.sp, color: const Color(0xFFFFFFFF))),
       backgroundColor: Get.theme.primaryColor,
       centerTitle: true,
       elevation: elevation ?? 0,
-      leading: IconButton(icon: const Icon(Icons.arrow_back_ios, color: Colors.white, size: 22), onPressed: () => Get.back()),
+      leading: leading ?? IconButton(icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 22), onPressed: () => Get.back()),
       actions: actions,
       bottom: bottom,
     );
