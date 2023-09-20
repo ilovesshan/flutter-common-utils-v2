5704e5a3255da70da4a17bf544daf3d118142e56
ilovesshan <ilovesshany@gmail.com>
2023-09-20 22:45:51 +0800

    refactor: merge commonAppBar function

--- lib/src/widgets/common_app_bar.dart
+++ lib/src/widgets/common_app_bar.dart
@@ -4,29 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:common_utils_v2/common_utils_v2.dart';
 
 class CommonAppBar {
-  /// 简朴白
-  static AppBar show(String text, {Widget? leading, List<Widget>? actions, PreferredSizeWidget? bottom, double? elevation}) {
+  static AppBar show(String text,
+      {Widget? leading, List<Widget>? actions, PreferredSizeWidget? bottom, double? elevation, Color? color, Color? backgroundColor, Color? leadingColor, bool? centerTitle, Brightness? statusBarIconBrightness}) {
     return AppBar(
-      systemOverlayStyle: const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.dark),
-      title: Text(text, style: TextStyle(fontSize: 18.sp, color: const Color(0xFF222222))),
-      backgroundColor: Colors.white,
-      centerTitle: true,
+      systemOverlayStyle: SystemUiOverlayStyle(statusBarIconBrightness: statusBarIconBrightness ?? Brightness.light),
+      title: Text(text, style: TextStyle(fontSize: 18.sp, color: color ?? const Color(0xFFFFFFFF))),
+      backgroundColor: backgroundColor ?? Get.theme.primaryColor,
+      centerTitle: centerTitle ?? true,
       elevation: elevation ?? 0,
-      leading: leading ?? IconButton(icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 22), onPressed: () => Get.back()),
-      actions: actions,
-      bottom: bottom,
-    );
-  }
-
-  /// 主题色
-  static AppBar showWidthPrimaryTheme(String text, {Widget? leading, List<Widget>? actions, PreferredSizeWidget? bottom, double? elevation}) {
-    return AppBar(
-      systemOverlayStyle: const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.light),
-      title: Text(text, style: TextStyle(fontSize: 18.sp, color: const Color(0xFFFFFFFF))),
-      backgroundColor: Get.theme.primaryColor,
-      centerTitle: true,
-      elevation: elevation ?? 0,
-      leading: leading ?? IconButton(icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 22), onPressed: () => Get.back()),
+      leading: leading ?? IconButton(icon: Icon(Icons.arrow_back_ios, color: leadingColor ?? Colors.black, size: 22), onPressed: () => Get.back()),
       actions: actions,
       bottom: bottom,
     );
